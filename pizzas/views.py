7dd1b97c87b10b6878285eb0cac7e37cdcd7c9a7
Victoria Donohoo <victoria_donohoo1@baylor.edu>
2021-05-02 11:52:43 -0500

    fixed it

--- pizzas/views.py
+++ pizzas/views.py
@@ -7,13 +7,13 @@ def index(request):
     return render(request, 'pizzas/index.html')
 
 def pizzas(request):
-    pizzas = Pizza.objects
+    pizzas = Pizza.objects.order_by('name')
     context = {'pizzas': pizzas}
     return render(request, 'pizzas/pizzas.html', context)
 
 def pizza(request, pizza_id):
     pizza = Pizza.objects.get(id=pizza_id)
-    toppings = pizza.name_set
+    toppings = pizza.topping_set.order_by('pizza')
 
     context = {'pizza': pizza, 'toppings': toppings}
     return render(request, 'pizzas/pizza.html', context)
